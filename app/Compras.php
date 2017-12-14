e104705a88eef401f7c04a5c3813b6a728b900e5
Daniel Rodriguez <dannyjose1112@hotmail.com>
2017-12-13 18:41:34 -0600

    [UPDATED] added joins for compras model

--- app/Compras.php
+++ app/Compras.php
@@ -9,4 +9,16 @@ class Compras extends Model
 {
     use SoftDeletes;
     protected $table = 'compras';
+    
+    public function detalle(){
+        return $this->hasMany('App\ComprasDetalle','compra','id')->with('productos');
+    }
+
+    public function proveedores(){
+        return $this->hasOne('App\Proveedores','id','proveedor');
+    }
+
+    public function tipos(){
+        return $this->hasOne('App\TiposCompra','id','tipo');
+    }
 }
